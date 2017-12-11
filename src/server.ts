cb28e2249b9086f209a7dc87494399a83485833a
Jace Patel <jace.patel@gmail.com>
2017-12-11 21:31:31 +1000

    Add the first funciton and the tslint file

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
-function go() {
-  return `Hello!`
+import * as prices from "./services/prices";
+
+async function go() {
+  let currentBitcoinPrice = await prices.getBtcAudPrice();
+  console.log("The current bitcoin price is ", currentBitcoinPrice);
 }
 
-console.log(go())
+go();
---
+++ src/services/prices.ts
@@ -0,0 +1,19 @@
+import * as config from "config"
+import * as rp from "request-promise"
+
+const API_KEY = config.get("BTCMarkets.apiKey")
+const SECRET_KEY = config.get("BTCMarkets.secretKey")
+const BASE_URL = config.get("BTCMarkets.url")
+
+var options = {
+    uri: "",
+    method: "GET",
+    json: true
+};
+
+export async function getBtcAudPrice() {
+    let requestOptions = options;
+    requestOptions.uri = `${BASE_URL}/market/BTC/AUD/tick`;
+    let currentPriceInformation = await rp(options)
+    return currentPriceInformation;
+}
