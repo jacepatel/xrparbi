fac3127e30b222c11c5e57a2735bfb45c25b15c9
Jace Patel <jace.patel@gmail.com>
2017-12-11 21:55:27 +1000

    Update the buy / sell cycle comments

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,14 @@ go();
 
 
 // General gist
+// If you have AUD in Balance without orders then enter the buy cycle
+
+// SELL CYCLE
+// Sell XRP at top of channel if you have XRP available
+// If XRP > BTC > AUD is more than the middle of buy / sell then sell via that
+
+// BUY CYCLE
 // Set the buy / sell channel ie buy at 33, sell at 34
 // If external price dips then sell at buy price
 // If external price rises then hedge and cancel half, dollar cost average on the way up if it goes
 // If AUD > BTC > XRP + 0.52% is less than the middle of buy / sell then buy
-// If XRP > BTC > AUD is more than the middle of buy / sell then sell
