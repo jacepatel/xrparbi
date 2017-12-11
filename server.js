83912213c0c43fa5a13388e7cc8004a796992ab3
Jace Patel <jace.patel@gmail.com>
2017-12-11 20:52:15 +1000

    Get some files and app in play

---
+++ server.js
@@ -0,0 +1,5 @@
+function greeter(person) {
+    return "Hello " + person + "!";
+}
+var appName = 'Node Hero';
+console.log(greeter(appName));
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+function go() {
+  return `Hello!`
+}
+
+console.log(go())
